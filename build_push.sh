5bd75583fa95244a4ae6682774fb8a1a571946e9
Gustavo Cunha <gustavocunha182020@gmail.com>
2024-12-29 23:03:07 +0000

    Instalação do curl no container do modelo

--- build_push.sh
+++ build_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t gustavofcunha/recommendation_rules:v3 -f Model/Dockerfile.model .
-docker push gustavofcunha/recommendation_rules:v3
-docker build -t gustavofcunha/rest_api_server:v3 -f APIs/Dockerfile.api .
-docker push gustavofcunha/rest_api_server:v3
+docker build -t gustavofcunha/recommendation_rules:v4 -f Model/Dockerfile.model .
+docker push gustavofcunha/recommendation_rules:v4
+docker build -t gustavofcunha/rest_api_server:v4 -f APIs/Dockerfile.api .
+docker push gustavofcunha/rest_api_server:v4
